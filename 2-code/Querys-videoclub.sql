8546258e2dc83b6a0c09081d6e8126b7d91eb571
Davidmcii <davidmartincasero@hotmail.com>
2022-08-26 22:16:29 +0200

    ejercicio completado

---
+++ 2-code/Querys-videoclub.sql
@@ -0,0 +1,166 @@
+-- cantidad de peliculas clasificadas por rating
+
+select count(rating) as quantity, rating
+from filmactor as fa
+left join actor as a
+on a.actor_id=fa.actor_id
+left join film as f
+on f.film_id=fa.film_id
+group by rating
+order by rating asc;
+
+-- Actores por película
+
+select count(a.actor_id) as cuenta, a.complete_name 
+from filmactor as fa
+left join actor as a
+on a.actor_id=fa.actor_id
+left join film as f
+on f.film_id=fa.film_id
+group by a.actor_id
+
+order by cuenta desc;
+
+-- peliculas con más actores
+
+select count(f.film_id) as cuenta, f.title 
+from filmactor as fa
+left join actor as a
+on a.actor_id=fa.actor_id
+left join film as f
+on f.film_id=fa.film_id
+group by f.film_id
+
+order by cuenta desc;
+
+-- Cuantas películas hay de cada lengua
+
+select count(f.title) as cuenta, lan.name
+from filmactor as fa
+left join actor as a
+on a.actor_id=fa.actor_id
+left join film as f
+on f.film_id=fa.film_id
+left join language as lan
+on f.language_id= lan.language_id 
+group by f.language_id
+;
+
+-- Cliente más alquilador
+
+select r.customer_id, count(title) as mas_alquilador
+
+from rental as r
+left join inventory as i
+on i.inventory_id=r.inventory_id
+left join film as f
+on f.film_id=i.film_id
+
+group by r.customer_id
+
+order by mas_alquilador desc;
+
+
+-- pelicula más alquilada
+
+select count(f.title) as mas_alquiladas, f.title
+
+from rental as r
+left join inventory as i
+on i.inventory_id=r.inventory_id
+left join film as f
+on f.film_id=i.film_id
+
+group by f.film_id
+order by mas_alquiladas desc
+;
+
+-- dinero generado por cada pelicula
+
+select sum(rental_rate)  as ganancias_per_film, f.title
+
+from rental as r
+left join inventory as i
+on i.inventory_id=r.inventory_id
+left join film as f
+on f.film_id=i.film_id
+
+group by f.film_id
+order by ganancias_per_film desc
+;
+
+-- dinero generado por cada actor
+
+select sum(f.rental_rate) as ganancias_per_actor, a.complete_name
+from filmactor as fa
+left join actor as a
+on a.actor_id=fa.actor_id
+left join film as f
+on f.film_id=fa.film_id
+left join inventory as i
+on f.film_id=i.film_id
+left join rental as r
+on i.inventory_id=r.inventory_id
+group by a.actor_id
+order by ganancias_per_actor desc
+;
+
+
+--  5 actores más alquilados por veces
+
+select a.complete_name, count(a.complete_name) as actor_mas_alquilado
+
+from rental as r 
+left join inventory as i
+on i.inventory_id=r.inventory_id
+left join film as f
+on f.film_id=i.film_id
+left join filmactor as fa
+on fa.film_id=i.film_id
+left join actor as a
+on a.actor_id=fa.actor_id
+
+group by  complete_name 
+
+order by actor_mas_alquilado desc limit 5;
+
+-- varias peliculas sin actores, comprobación
+select *
+from film as f
+left join filmactor as fa
+on fa.film_id=f.film_id
+
+ where title="balloon homeward" or title= "beauty grease" or title = "crazy home";
+
+-- categorias de mas titulos hay
+
+select count(f.title) as pelis_per_cat, cat.category_id, cat.name
+from category as cat
+left join filmactor as fa
+on fa.category_id=cat.category_id
+left join film as f
+on f.film_id=fa.film_id
+
+group by category_id
+;
+
+-- mas alquilados por categoria
+
+select count(cat.name) as cat_mas_alq, cat.name
+from category as cat
+left join filmactor as fa
+on fa.category_id=cat.category_id
+left join film as f
+on f.film_id=fa.film_id
+left join inventory as i
+on f.film_id=i.film_id
+left join rental as r
+on i.inventory_id=r.inventory_id
+where r.rental_id>0
+
+group by cat.name
+
+order by cat_mas_alq desc
+;
+
+
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # DMC-w3-database-project
 DMC SQL database
+
+## 1- Introducción Proyecto W3-database-project
+
+El proyecto trata de crear una base de datos de un videoclub partiendo de algunas de las tablas CSV que podrían formar previamente una base de datos. Algunos de los datos están preparados para una base de datos mientras que algunas de las partes relacionales están en un formato antiguo. 
+
+## 2- Proceso del trabajo.
+
+1- se cargaron las CSVs a python
+2- se crearon 2 funciones para evaluar los datos. 
+3- se limpiaron las tablas dejandolas listas para ejecutarlas. Se eliminaron los nulos y se cambió algunos de los formatos de los archivos. 
+4- se juntaron las tablas de actores y títulos a través de la vieja hoja de registros. 
+5- se cambió en un random la tabla inventory ID.
+6- se creo la database y se exportaron las tablas a SQL
+7-en SQL se definieron las conexiones entre las tablas
+8- se lanzaron querys para extraer datos de las tablas. 
+
+
+## 3- Organización de la documentación. 
+
+Se organizan los archivos en 3 carpetas: 
+
+1-Raw_data: Donde se recogen los documentos en blanco
+2-code: donde se encuentra los ejecutables tanto de python como de SQL para formar la documentación
+3-Output: se encuentan los documentos para generar la base de datos ya con las conexiones hechas. 
+
